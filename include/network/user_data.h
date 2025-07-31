40b14a7d82a1e9223be4abc4821a3538656a3bf2
lgferreiracic <lgferreira.cic@uesc.br>
2025-07-30 22:46:53 -0300

    Primeiro Commit

--- include/network/user_data.h
+++ include/network/user_data.h
@@ -3,7 +3,7 @@
 
 #define WIFI_SSID "Residencia"        // Substitua pelo nome da sua rede Wi-Fi
 #define WIFI_PASSWORD "embarcatech"   // Substitua pela senha da sua rede Wi-Fi
-#define MQTT_SERVER "192.168.1.107"   // Substitua pelo endereço do host - broket MQTT: Ex: 192.168.1.107
+#define MQTT_SERVER "000.000.0.000"   // Substitua pelo endereço do host - broket MQTT: Ex: 000.000.0.000
 #define MQTT_USERNAME "embarcatech"   // Substitua pelo nome da host MQTT - Username
 #define MQTT_PASSWORD "embarcatech"   // Substitua pelo Password da host MQTT - credencial de acesso - caso exista
 
